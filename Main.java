9cb3d8ca539670bcf58246715a7641bc4c3516c9
charul16yadav <95537438+charul16yadav@users.noreply.github.com>
2024-10-07 22:33:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //------------->BASICS<-------------
+        // int a = 10;
+        // int b = 25;
+        // int sum = a + b;
+        // System.out.println(sum);
+        // int diff = b - a;
+        // System.out.println(diff);
+        // int mul = a * b ;
+        // System.out.println(mul);
+
+        //Scanner sc = new Scanner(System.in);// Scanner sc is used for taking input 
+        //String name = sc.nextLine(); //sc.next ------> is used for first token only
+        //sc.nextLine ---->is used for full line
+        //nextInt -------->integer type input
+        //nextFloat.......
+        //System.out.println(name);
+    }
+    
+}
