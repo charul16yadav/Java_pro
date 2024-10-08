c55facb117d05fdc6003145f97d46b1c87acba3c
charul16yadav <95537438+charul16yadav@users.noreply.github.com>
2024-10-08 12:35:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,12 +13,33 @@ public class Main {
         // int mul = a * b ;
         // System.out.println(mul);
 
-        //Scanner sc = new Scanner(System.in);// Scanner sc is used for taking input 
-        //String name = sc.nextLine(); //sc.next ------> is used for first token only
-        //sc.nextLine ---->is used for full line
-        //nextInt -------->integer type input
-        //nextFloat.......
+        //Scanner sc = new Scanner(System.in);    // --->Scanner sc is used for taking input 
+        //String name = sc.nextLine();         //sc.next ------> is used for first token only
+                                              //sc.nextLine ---->is used for full line
+                                              //nextInt -------->integer type input
+                                              //nextFloat.............and so on 
         //System.out.println(name);
+        
+        //------------>for loop <----------
+        //for(int counter = 0; counter < 5; counter = counter + 1 ) {
+
+        //for (int i = 0; i < 10; i++) {
+        //for (initialization; condition; updation){}
+        
+        //   System.out.println("Hello World");
+       // }
+        //---------->While loop <--------------
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i++; // i++;
+        // }
+        //------->Do while Loop<---------------
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i = i + 1;
+        // }w
     }
     
 }
