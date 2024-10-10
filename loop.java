416dfda3874d5d249642439a727abb974231a775
Charu <95537438+charul16yadav@users.noreply.github.com>
2024-10-10 22:01:09 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,29 @@
+// LOOPS
+//->For loops, while loops, do while loop
+
+
+public class loop {
+    public static void main(String args[]) {
+        //---->for
+        //for(int counter = 0; counter < 5; counter = counter + 1 ) {
+
+        //for (int i = 0; i < 10; i++) {
+        //for (initialization; condition; updation){}
+        
+        //   System.out.println("Hello World");
+        //}
+        //------->While
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i++; // i++;
+        // }
+        //------->Do while
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i = i + 1;
+        // }while(i < 11);
+        
+    }
+}
