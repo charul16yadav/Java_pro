75f5ba9f9745e528c56db1ec5cd23cda6b861150
Charu <95537438+charul16yadav@users.noreply.github.com>
2024-10-14 23:01:46 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -30,9 +30,23 @@ public class pattern {
         //     System.out.println();
         // }
 
-        //------------->
-        for(int i = 4; i>=0; i--){
-            for (int j = 1; j<=i; j++){
+        //------------->inverted pyramid<------------
+        // for(int i = 4; i>=0; i--){
+        //     for (int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+        //---------->
+        for(int i=1; i<=4; i++){
+            // inner loop -> Space print
+            for (int j=1; j<=4-i; j++ ){
+                System.out.print(" ");
+                
+            }
+            // inner loop -> star print
+            for(int j=1; j<=i; j++ ){
                 System.out.print("*");
             }
             System.out.println();
